d65609731fc28b6be359b1b191400bbfb9bfe76c
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2020-12-18 15:17:39 -0600

    commit tree 8bbbfb747ffa2aadeaa095b95d46a3fb870d8a4f

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,19 @@
 require 'pry'
 
 class Artist
+
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods  
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+  # def initialize
+  #   @@artists << self
+  #   @songs = []
+  # end
 
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
@@ -19,13 +23,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
